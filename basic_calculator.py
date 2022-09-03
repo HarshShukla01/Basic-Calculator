d709a8358d577e766515098889887d705169e4e0
Harsh Shukla <112650135+HarshShukla01@users.noreply.github.com>
2022-09-04 01:26:24 +0530

    Add files via upload

---
+++ basic_calculator.py
@@ -0,0 +1,57 @@
+
+import sys
+
+# -------------------------------------------------------- #
+# ---------------- CALCULATOR FUNCTIONS ------------------ #
+# -------------------------------------------------------- #
+
+# Add function
+def add(a, b):
+    return a + b
+
+# Subtract function
+def sub(a, b):
+    return a - b
+
+# Multiply function
+def mult(a, b):
+    return a * b
+
+# Divide function
+def div(a, b):
+    return a / b
+
+
+
+a = None
+b = None
+op = None
+
+while (True):
+    # get input values
+    a = input("Enter the first argument: ")
+    op = input("Enter the operation: ")
+    b = input("Enter the second argument: ")
+    try:
+        a = int(a)
+        b = int(b)
+    except ValueError:
+        print("Invalid number argument...")
+        op = None
+
+    # decide function
+    if (op != None):
+        if (op == "+"):
+            print("Sum: ", add(a, b))
+        elif (op == "-"):
+            print("Difference: ", sub(a, b))
+        elif (op == "*"):
+            print("Product: ", mult(a, b))
+        elif (op == "/"):
+            print("Quotient: ", div(a, b))
+        else:
+            print("Invalid operation...")
+
+    q = input("Quit? [y/n] ")
+    if (q == "y" or q == "Y"):
+        break
